54da6a8574d3a6b61ba503e3120f88d6f79cdda3
ankit <ankit-kumar.kanujia@capgemini.com>
2023-04-28 16:23:03 +0530

    Added GatewayServer

---
+++ gateway-server/src/test/java/org/jfs/drivein/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jfs.drivein.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
